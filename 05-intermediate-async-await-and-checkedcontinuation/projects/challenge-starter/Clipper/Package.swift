157a93cda65bc77418db15df8f26a49245b0cd1e
Stijn Willems <stijn@dooz.be>
2023-03-13 10:19:42 +0100

    make clipper work on linux

--- 05-intermediate-async-await-and-checkedcontinuation/projects/challenge-starter/Clipper/Package.swift
+++ 05-intermediate-async-await-and-checkedcontinuation/projects/challenge-starter/Clipper/Package.swift
@@ -6,7 +6,8 @@ let package = Package(
   name: "Clipper",
   platforms: [.macOS(.v12)],
   dependencies: [
-    .package(url: "https://github.com/Zollerboy1/SwiftCommand.git", from: "1.1.2")
+    // open PR to fix this [Fix build on linux by adding a missing #else #1](https://github.com/Zollerboy1/SwiftCommand/pull/1)
+    .package(url: "https://github.com/doozMen/SwiftCommand.git", branch: "main")
   ],
   targets: [
     .executableTarget(
